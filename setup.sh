c093dbf8df75b460cbd59f035f7e9ab1af991521
Zech Bron <39665654+ZechBron@users.noreply.github.com>
2020-09-03 14:06:38 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+pkg install termux-api -y
+pkg install termux-tools -y
+
+chmod +x termuxSMS.sh
+
+pm list packages -3 > log
+grep "package:com.termux.api" log
+if [ $? = 0 ]; then
+   echo -e "\e[32mSetting up done.\e[0m"
+elif [ $? = 1 ]; then
+   echo -e "\e[91mYou don't have termux-api app.\nPlease install it first here: \e[0m"
+   echo "   https://m.apkpure.com/termux-api/com.termux.api/download/31-APK"
+fi
+rm -f log
+
